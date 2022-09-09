0339e1ef5910e871e6aa8ade136b07719995ea8e
smallbenji <smallbenji@LenWin.localdomain>
2022-09-09 12:21:49 +0200

    a very small die percent

--- src/components/mainPage/mainPage.jsx
+++ src/components/mainPage/mainPage.jsx
@@ -1,14 +1,16 @@
-import { React, useEffect} from 'react'
+import { React, useEffect, useState} from 'react'
 import {Button} from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import './mainPage.scss'
 
 export default function MainPage() {
 
+  var [varaible, setVariable] = useState();
   useEffect(() => {
-    
+    if(varaible != undefined) return;
     fetch('https://carolinehair.herokuapp.com/getAll')
       .then(response => response.json())
+      .then(json => setVariable(json))
       .then(json => console.log(json));
   });
   
@@ -16,11 +18,7 @@ export default function MainPage() {
     <div className='mainPage'>
         <div className='left'>
             <Button variant='contained'><h1>Hello there!</h1></Button>
-            <DataGrid
-              pageSize={5}
-              rowsPerPageOptions={[5]}
-              checkboxSelection
-            />
+            {varaible ? <h1>{varaible[1].title}</h1> : <h1>Loading...</h1>}
         </div>
         <div className='right'>
             <h1>right</h1>

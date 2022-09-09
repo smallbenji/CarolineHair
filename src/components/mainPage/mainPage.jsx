5b52720497bea3611dee4f6a3706431280cdff56
smallbenji <smallbenji@LenWin.localdomain>
2022-09-09 11:09:05 +0200

    lowkey wanna die

--- src/components/mainPage/mainPage.jsx
+++ src/components/mainPage/mainPage.jsx
@@ -1,20 +1,26 @@
-import {Component, React, useEffect} from 'react'
+import { React, useEffect} from 'react'
+import {Button} from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
 import './mainPage.scss'
-import axios from 'axios'
 
 export default function MainPage() {
 
   useEffect(() => {
     
-    axios.get('http://carolinehair.herokuapp.com/getAll')
-    .then(res => console.log(res.data));
-
+    fetch('https://carolinehair.herokuapp.com/getAll')
+      .then(response => response.json())
+      .then(json => console.log(json));
   });
   
   return (
     <div className='mainPage'>
         <div className='left'>
-            <h1>left</h1>
+            <Button variant='contained'><h1>Hello there!</h1></Button>
+            <DataGrid
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+            />
         </div>
         <div className='right'>
             <h1>right</h1>

40eaa55b4c88093bc426c186ce44402ee74d496d
smallbenji <smallbenji@LenWin.localdomain>
2022-09-09 13:17:26 +0200

    i wanna die

---
+++ src/components/bookingList/bookingList.jsx
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+import './bookingList.scss';
+
+export default function BookingList(){
+    return(
+        <div className='bookingList'>
+            <h1>List</h1>
+        </div>
+    )
+}
--- src/components/mainPage/mainPage.jsx
+++ src/components/mainPage/mainPage.jsx
@@ -2,13 +2,14 @@ import { React, useEffect, useState} from 'react'
 import {Button} from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import './mainPage.scss'
+import BookingList from '../bookingList/bookingList';
 
 export default function MainPage() {
 
   var [varaible, setVariable] = useState();
   useEffect(() => {
     if(varaible != undefined) return;
-    fetch('https://carolinehair.herokuapp.com/getAll')
+    fetch('/getAll')
       .then(response => response.json())
       .then(json => setVariable(json))
       .then(json => console.log(json));
@@ -18,6 +19,7 @@ export default function MainPage() {
     <div className='mainPage'>
         <div className='left'>
             <Button variant='contained'><h1>Hello there!</h1></Button>
+            <BookingList />
             {varaible ? <h1>{varaible[1].title}</h1> : <h1>Loading...</h1>}
         </div>
         <div className='right'>

f26401deff1ecc9d3291e4644ddafd760f81167a
smallbenji <smallbenji@LenWin.localdomain>
2022-09-10 00:50:55 +0200

    i low key wanna die

--- src/components/mainPage/mainPage.jsx
+++ src/components/mainPage/mainPage.jsx
@@ -1,6 +1,5 @@
 import { React, useEffect, useState} from 'react'
 import {Button} from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid'
 import './mainPage.scss'
 import BookingList from '../bookingList/bookingList';
 
@@ -8,8 +7,8 @@ export default function MainPage() {
 
   var [varaible, setVariable] = useState();
   useEffect(() => {
-    if(varaible != undefined) return;
-    fetch('https://carolinehair.herokuapp.com/getAll')
+    if(varaible !== undefined) return;
+    fetch('http://carolinehair.herokuapp.com/getAll')
       .then(response => response.json())
       .then(json => setVariable(json))
       .then(json => console.log(json));
@@ -18,13 +17,14 @@ export default function MainPage() {
   return (
     <div className='mainPage'>
         <div className='left'>
-            <Button variant='contained'><h1>Hello there!</h1></Button>
-            <BookingList />
-            {varaible ? <h1>{varaible[1].title}</h1> : <h1>Loading...</h1>}
+        {varaible.map((item, index) => (
+              <h1>{item.title}</h1>
+            ))}
         </div>
         <div className='right'>
-            <h1>right</h1>
-        </div>
+        <Button variant='contained'><h1>Hello there!</h1></Button>
+        <BookingList />
+        {varaible ? <h1>{varaible[0].title}</h1> : <h1>Loading...</h1>}        </div>
     </div>
   )
 }
